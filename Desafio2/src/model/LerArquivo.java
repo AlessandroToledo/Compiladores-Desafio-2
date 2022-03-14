23e2cc290832c34b0349e6e74144fc6da4ebacdb
alekin <57714182+AlessandroToledo@users.noreply.github.com>
2022-03-14 15:16:44 -0300

    Add files via upload

---
+++ Desafio2/src/model/LerArquivo.java
@@ -0,0 +1,97 @@
+package model;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class LerArquivo {
+	public static void main(String[] args) {
+		
+		Pilha pilha = new Pilha();
+
+	    try {
+	    	
+	      FileReader arq = new FileReader("prog.txt");
+	      BufferedReader lerArq = new BufferedReader(arq);      
+	      String validacao="";
+	      String linha = lerArq.readLine();
+	      FileWriter arqW = new FileWriter("prog-check.txt");
+	      PrintWriter gravarArq = new PrintWriter(arqW);
+	     
+	      while (linha != null) {
+	    	  
+	    	  int tamlinha=linha.length();
+	    	  
+	    	  for(int i=0;i<tamlinha;i++)
+	    	  {
+	    		  if(linha.charAt(i)=='(' || linha.charAt(i)=='{' || linha.charAt(i)=='[' ||linha.charAt(i)=='<')
+	    		  {
+	    			  pilha.empilhar(linha.charAt(i));
+	    			  System.out.println("empilha");
+	    		  }
+	    		  else if(pilha.pilhaVazia()==false)
+	    		  {
+	    			  if(linha.charAt(i)==')' && pilha.exibeUltimoValor()=='(')
+	    			  {
+	    				  pilha.desempilhar();
+	    				  System.out.println("desempilha");
+	    			  }
+		    		  else if(linha.charAt(i)=='}' && pilha.exibeUltimoValor()=='{')
+		    		  {
+		    			  pilha.desempilhar();
+		    			  System.out.println("desempilha");
+		    		  }
+		    		  else if(linha.charAt(i)==']' && pilha.exibeUltimoValor()=='[')
+		    		  {
+		    			  pilha.desempilhar();
+		    			  System.out.println("desempilha");
+		    		  }
+		    		  else if(linha.charAt(i)=='>' && pilha.exibeUltimoValor()=='<')
+		    		  {
+		    			  pilha.desempilhar();
+		    			  System.out.println("desempilha");
+		    		  }
+		    		  else
+		    		  {
+		    			  validacao=" - Inválido";
+		    			  System.out.println("erro");
+		    		  }
+	    		  }
+	    		  else
+	    		  {
+	    			  validacao=" - Inválido";
+	    			  break;
+	    		  }
+	    		
+	    		  if(i==tamlinha-1 && pilha.pilhaVazia()==true)
+	    		  {
+	    			  validacao=" - OK";	    	
+	    		  }
+	    		  if(i==tamlinha-1 && pilha.pilhaVazia()==false)
+	    		  {
+	    			  validacao=" - Inválido";	    	
+	    		  }	    		  	    		  
+	    		  
+	    	  }
+	    	  
+	    	  gravarArq.printf(linha+validacao+"%n");   	  
+	    	  pilha.limparPilha();
+	    	  linha = lerArq.readLine();
+	        
+	      }
+
+	      arq.close();
+	      arqW.close();
+	      
+	    } catch (IOException e) {
+	        System.err.printf("Erro na abertura do arquivo: %s.\n",
+	          e.getMessage());
+	    }
+
+	    System.out.println();
+	    
+	  }
+
+}
